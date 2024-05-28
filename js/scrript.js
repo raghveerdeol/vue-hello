ca1ba9fa374ef95d71a6ec5f65a407e5f81d04b9
raghveerdeol <deolraghveer5@gmail.com>
2024-05-28 18:19:03 +0200

    aggiunto attributo al click del immagine

--- js/scrript.js
+++ js/scrript.js
@@ -7,5 +7,12 @@
         message: 'Ciao Mondo!',
         image: "https://content.imageresizer.com/images/memes/Batman-Gasp-meme-8.jpg"
     }
+    },
+    methods: {
+        rosso: function () {
+        const h1El = document.querySelector("h1");
+        h1El.classList.add("red");
+        }
     }
+    
     }).mount('#app')
